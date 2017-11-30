8754143283bef4d7708a26bdbed778ef19a12419
Manasi Joshi <manasijoshi1993@gmail.com>
2017-11-30 05:43:51 -0800

    Updating controller file

--- CMPE275_TermProject/src/main/java/edu/sjsu/cmpe275/termproject/Cmpe275TermProjectApplication.java
+++ CMPE275_TermProject/src/main/java/edu/sjsu/cmpe275/termproject/Cmpe275TermProjectApplication.java
@@ -1,8 +1,11 @@
 package edu.sjsu.cmpe275.termproject;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Cmpe275TermProjectApplication {
 
--- CMPE275_TermProject/src/main/java/edu/sjsu/cmpe275/termproject/controllers/CUSRController.java
+++ CMPE275_TermProject/src/main/java/edu/sjsu/cmpe275/termproject/controllers/CUSRController.java
@@ -1,14 +1,19 @@
 package edu.sjsu.cmpe275.termproject.controllers;
 
-//@RestController("/")
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
 public class CUSRController {
 
 
-	//@Autowired
+	@Autowired
 	private CUSRController cusrcontroller;
 	
 	
-	//@GetMapping(value="/")
+	@RequestMapping("/")
 	public String sayHello()
 	{
 		return "Hello!";
